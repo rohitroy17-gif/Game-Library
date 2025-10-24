9a6941db53dfed21e7d069f7297afe816d67a074
rohitroy17-gif <roy172758@gmail.com>
2025-10-24 12:37:32 +0600

    width set

--- src/HomePage.jsx
+++ src/HomePage.jsx
@@ -7,14 +7,14 @@ import { Helmet } from 'react-helmet-async';
 const HomePage = () => {
   return (
     <>
-    <div  className="container text-center text-red-400 text-3xl mx-auto p-4 bg-gray-800
+    <div  className="container text-center text-red-400 text-3xl  max-w-full p-4 bg-gray-800
 ">
       <Helmet>
         <title>Home | Gamehub</title>
       </Helmet>
       <b><h1>Welcome to Gamehub</h1></b>
     </div>
-    <div className="container mx-auto p-4 bg-gradient-to-r from-gray-900 via-gray-800 to-gray-700">
+    <div className="container max-w-full p-4 bg-gradient-to-r from-gray-900 via-gray-800 to-gray-700">
       <Banner />
       <PopularGames />
       <Newsletter />
