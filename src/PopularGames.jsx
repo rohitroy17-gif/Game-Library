ad09dcc2d02aa34bfb90dbe7cdbd9cc51f6b8f84
rohitroy17-gif <roy172758@gmail.com>
2025-10-24 12:45:07 +0600

    bg color done home

--- src/PopularGames.jsx
+++ src/PopularGames.jsx
@@ -8,16 +8,16 @@ const PopularGames = () => {
   const sortedGames = [...gamesData].sort((a, b) => b.ratings - a.ratings).slice(0, 3);
 
   return (
-    <div className="grid grid-cols-3 gap-4 mt-8">
+    <div className="grid grid-cols-3 p-4 gap-4 mt-8 bg-gray-800">
       {sortedGames.map(game => (
         <div 
           key={game.id} 
-          className="border p-4 rounded cursor-pointer hover:shadow-lg"
+          className="border p-4 rounded-2xl cursor-pointer bg-gray-600 hover:shadow-lg"
           onClick={() => navigate(`/games/${game.id}`)}
         >
           <img src={game.coverPhoto} alt={game.title} className="w-full h-40 object-cover rounded"/>
-          <h2 className="font-bold mt-2">{game.title}</h2>
-          <p>Rating: {game.ratings}</p>
+          <h2 className="font-bold mt-2 text-red-800">{game.title}</h2>
+          <p className='text-yellow-800'>Rating: {game.ratings}</p>
         </div>
       ))}
     </div>
